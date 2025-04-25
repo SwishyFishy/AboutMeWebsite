685fe44f3959c4a36333aa6127a4812dd7e6fb26
SwishyFishy <jonahcoyotegf@icloud.com>
2025-04-25 15:41:28 -0400

    Header and footer

---
+++ MyWebsite/src/components/page/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer()
+{
+    return(
+        <div id="Footer">
+            <span>&copy; 2025 Jonah Galloway-Fenwick. All Rights Reserved.</span>
+        </div>
+    );
+}
+
+export default Footer;
--- MyWebsite/src/routes/Layout.tsx
+++ MyWebsite/src/routes/Layout.tsx
@@ -1,9 +1,15 @@
 import { Outlet } from "react-router";
+import Header from "../components/page/Header";
+import Footer from "../components/page/Footer";
 
 function Layout()
 {
     return(
-        <Outlet />
+        <>
+            <Header/>
+            <Outlet/>
+            <Footer/>
+        </>
     );
 }
 
