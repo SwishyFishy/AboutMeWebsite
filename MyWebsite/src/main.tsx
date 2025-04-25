015cea1938dd6f9afc9855743b383df7df0b80b1
SwishyFishy <jonahcoyotegf@icloud.com>
2025-04-25 15:41:38 -0400

    Formatting

--- MyWebsite/src/main.tsx
+++ MyWebsite/src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-import { createBrowserRouter } from 'react-router'
-import { RouterProvider } from 'react-router'
-import routes from './routes/routes.tsx'
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from 'react-router';
+import routes from './routes/routes.tsx';
 
-import './styles/main.css'
+import './styles/main.css';
 
 const router = createBrowserRouter(routes);
 
@@ -13,4 +13,4 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
-)
+);
