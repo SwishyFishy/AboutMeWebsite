ae92197706aa84f68ccee2333f1e517ade95b035
SwishyFishy <jonahcoyotegf@icloud.com>
2025-04-26 15:30:22 -0400

    Error page style

--- MyWebsite/src/routes/ErrorPage.tsx
+++ MyWebsite/src/routes/ErrorPage.tsx
@@ -1,8 +1,17 @@
+import Footer from '../components/page/Footer';
+import Header from '../components/page/Header';
+
 import '../styles/ErrorPage.css';
 
 function ErrorPage()
 {
-    return(<p>Error 404 - Page Not Found</p>);
+    return(
+        <div id="layout">
+            <Header/>
+            <p>Error 404 - Page Not Found</p>
+            <Footer/>
+        </div>
+    );
 }
 
 export default ErrorPage;
