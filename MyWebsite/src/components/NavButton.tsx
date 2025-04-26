7e5992f777e145e2893265658a2917452e503dcc
SwishyFishy <jonahcoyotegf@icloud.com>
2025-04-25 23:21:29 -0400

    NavButton component for header buttons

---
+++ MyWebsite/src/components/NavButton.tsx
@@ -0,0 +1,13 @@
+type props_NavButton = {
+    value: string;
+    onClick: Function;
+}
+
+function NavButton(props: props_NavButton)
+{
+    return(
+        <input type="button" name="nav_button" id="nav_button" value={props.value} onClick={() => {props.onClick();}}></input>
+    );
+}
+
+export default NavButton;
--- MyWebsite/src/components/page/Header.tsx
+++ MyWebsite/src/components/page/Header.tsx
@@ -1,6 +1,17 @@
+import NavButton from "../NavButton";
+
 function Header()
 {
-    return(<p>Header</p>);
+    return(
+        <div id="header">
+            <nav>
+                <NavButton value="About Me" onClick={function(){}}/>
+                <NavButton value="My CV" onClick={function(){}}/>
+                <NavButton value="Small Projects" onClick={function(){}}/>
+                <NavButton value="Large Projects" onClick={function(){}}/>
+            </nav>
+        </div>
+    );
 }
 
 export default Header;
