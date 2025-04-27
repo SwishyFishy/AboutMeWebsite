3d81beaac7bd345737552239d0de8ce2826a57f4
SwishyFishy <jonahcoyotegf@icloud.com>
2025-04-26 22:01:07 -0400

    AboutMe content update

--- MyWebsite/src/routes/AboutMe.tsx
+++ MyWebsite/src/routes/AboutMe.tsx
@@ -10,7 +10,7 @@ function AboutMe()
             <div>
                 <h2>About Me</h2>
                 <p>
-                    Hi there! I'm Jonah, a recent Trent University computer science graduate and current full-time shift manager at JCI McDonalds in Peterborough, ON, Canada. This here's a little site I've been working on to show off myself, my projects, and my hobbies. The nav bar at the top can take you to my CV, contact information, and some project galleries.
+                    Hi there! I'm Jonah, a recent Trent University computer science graduate and current full-time shift manager at JCI McDonalds in Peterborough, ON, Canada. This here's a little site I've been working on to show off myself and my projects. The nav bar at the top can take you to my CV, contact information, and some project galleries.
                 </p>
                 <p>
                     Perhaps unsurprisingly, I enjoy programming. I'm especially fond of data management and algorithm design, but frontend and backend web development are both entertaining too. I've dabbled in machine learning before, and would like to do more of it. Beyond that, I like video games (mainly roguelikes and action/survial games) and Magic: the Gathering.
