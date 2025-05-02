e5576f663240214941177401b92a117f1b31e30f
SwishyFishy <jonahcoyotegf@icloud.com>
2025-05-02 16:30:51 -0400

    Text input component

---
+++ MyWebsite/src/components/FormTextInput.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+type props_FormTextInput = {
+    id: string,
+    placeholder: string;
+}
+
+function FormTextInput({id, placeholder}: props_FormTextInput)
+{
+    // Store input data in state
+    const [text, setText] = useState<string>("");
+    const handleInput = (e: any) => {
+        setText(e.currentTarget.value);
+    }
+
+    return(
+        <div id="form_text_input">
+            <label htmlFor={id}>Email:</label>
+            <input type="text" name={id} id={id} placeholder={placeholder} value={text} onChange={handleInput}/>
+        </div>
+    );
+}
+
+export default FormTextInput;
