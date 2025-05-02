aeab33078c7d76d96b69744c05cd1079e5781378
SwishyFishy <jonahcoyotegf@icloud.com>
2025-05-02 16:45:00 -0400

    Contact Me form

--- MyWebsite/src/routes/ContactMe.tsx
+++ MyWebsite/src/routes/ContactMe.tsx
@@ -1,6 +1,18 @@
+import FormTextInput from "../components/FormTextInput";
+import FormSubmit from "../components/FormSubmit";
+
 function ContactMe()
 {
-    return(<p>Hello ContactMe</p>);
+    return(
+        <div id="contact_me">
+            <h2>Contact Me</h2>
+            <form>
+                <FormTextInput label="Email" id="email" placeholder="Your email"/>
+                <FormTextInput label="Message" id="message" placeholder="Your message"/>
+                <FormSubmit value="Send" callback={(e: any) => {e.preventDefault(); console.log("called");}}/>
+            </form>
+        </div>
+    );
 }
 
 export default ContactMe;
