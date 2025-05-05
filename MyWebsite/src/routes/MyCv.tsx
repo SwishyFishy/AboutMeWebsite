369605ce2d4a85d80210af43947ae05dc3d1b65a
SwishyFishy <jonahcoyotegf@icloud.com>
2025-05-05 12:49:28 -0400

    Flesh out JCI work experience content

--- MyWebsite/src/routes/MyCv.tsx
+++ MyWebsite/src/routes/MyCv.tsx
@@ -70,6 +70,14 @@ function MyCv()
                     <p>
                         I started at JCI McDonalds immediately after graduating highschool in June 2021, delayed by the pandemic. After about a year and a half, I moved up to the Crew Trainer (or Junior Leader) position. There, I trained and verified at least 15 new hires on various stations. After this, I moved up to Area Lead, acting as a station leader for wherever I was positioned in the restaurant. In Janurary 2024, I was fully certified as a Shift Manager. While not the fastest progression, I spent all but the summers working part-time while in school at Trent, and was working full time otherwise.
                     </p>
+                    <h4>Responsibilities</h4>
+                    <ul>
+                        <li>Monitor the store for any safety hazards, either to crew, guests, or food, and react appropriately.</li>
+                        <li>Deliver positive service for all guests and handle complaints or disputes.</li>
+                        <li>Ensure that all crew are provided breaks without interrupting the flow of service for guests.</li>
+                        <li>Organize the completion of all tasks so that the next shift manager takes over a clean and stocked restaurant with no outstanding issues.</li>
+                        <li>Handle money from the tills and the safe to accurately tally and record daily sales.</li>
+                    </ul>
                 </div>
             </section>
             <section id="education">
