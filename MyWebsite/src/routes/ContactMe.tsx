a06324c53558ffd40a11e0b16a885b491289659e
SwishyFishy <jonahcoyotegf@icloud.com>
2025-05-09 12:57:19 -0400

    Non-submit button styling

--- MyWebsite/src/routes/ContactMe.tsx
+++ MyWebsite/src/routes/ContactMe.tsx
@@ -10,9 +10,10 @@ function ContactMe()
         <div id="contact_me"> 
             <h2>Contact Me</h2>
             <Form<{email: string, message: string}> initialValues={{email: '', message: ''}}>
+                <FormTextInput label="Name" id="name" placeholder="Your name" isArea={false}/>
                 <FormTextInput label="Email" id="email" placeholder="Your email" isArea={false}/>
                 <FormTextInput label="Message" id="message" placeholder="Your message" isArea={true}/>
-                <FormSubmit value="Send" submissionTrigger={(e: MouseEvent) => {e.preventDefault(); console.log("called");}}/>
+                <FormSubmit value="Send" submissionTrigger={(e: any) => {e.preventDefault(); console.log("called");}}/>
             </Form>
         </div>
     );
