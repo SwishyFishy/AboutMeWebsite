b5438a5e729ce4933ab748a9e453d0ff58ee5d06
SwishyFishy <jonahcoyotegf@icloud.com>
2025-05-12 18:06:16 -0400

    Editing pass

--- MyWebsite/src/routes/MyCv.tsx
+++ MyWebsite/src/routes/MyCv.tsx
@@ -7,10 +7,11 @@ function MyCv()
             <section id="me">
                 <h2>Core Attributes</h2>
                 <ul>
-                    <li><span>Outgoing</span></li>
-                    <li><span>Eager Learner</span></li>
-                    <li><span>Passionate</span></li>
+                    <li><span>Eager to Learn</span></li>
                     <li><span>Eye For Detail</span></li>
+                    <li><span>Planning and Organization</span></li>
+                    <li><span>Adaptable</span></li>
+                    <li><span>Reliable</span></li>
                 </ul>
             </section>
             <section id="skills">
@@ -22,17 +23,25 @@ function MyCv()
                     </li>
                     <li>
                         <span>First Aid</span>
-                        <span>Basic care + CPR</span>
+                        <span>Emergency Care + CPR</span>
                     </li>
                     <li>
                         <span>Management</span>
                     </li>
                     <li>
-                        <span>Coaching</span>
+                        <span>MS Office</span>
+                        <span>Word, Excel</span>
                     </li>
                     <li>
-                        <span>Web Development</span>
+                        <span>Instruction</span>
+                    </li>
+                    <li>
+                        <span>Frontend Web Dev</span>
                         <span>HTML, CSS, JavaScript, TypeScript, React, PHP</span>
+                    </li>
+                    <li>
+                        <span>Backend Web Dev</span>
+                        <span>Javascript, TypeScript, Express, PHP</span>
                     </li>
                     <li>
                         <span>Data Structures</span>
@@ -54,9 +63,6 @@ function MyCv()
                         <span>Git</span>
                     </li>
                     <li>
-                        <span>Excel</span>
-                    </li>
-                    <li>
                         <span>SQL</span>
                     </li>
                 </ul>
@@ -68,12 +74,13 @@ function MyCv()
                     <span>4 years</span>
                     <span>Shift Manager for 1.5 years</span>
                     <p>
-                        I started at JCI McDonalds immediately after graduating highschool in June 2021, delayed by the pandemic. After about a year and a half, I moved up to the Crew Trainer (or Junior Leader) position. There, I trained and verified at least 15 new hires on various stations. After this, I moved up to Area Lead, acting as a station leader for wherever I was positioned in the restaurant. In Janurary 2024, I was fully certified as a Shift Manager. While not the fastest progression, I spent all but the summers working part-time while in school at Trent, and was working full time otherwise.
+                        I started at JCI McDonalds in June 2021. After about a year and a half, I moved up to the Crew Trainer (or Junior Leader) position. There, I trained and verified at least 15 new hires on various stations. In Janurary 2024, I was fully certified as a Shift Manager. I worked full time during the summer months and 16-24h per week part-time while attending classes at University.
                     </p>
                     <h4>Responsibilities</h4>
                     <ul>
-                        <li>Monitor the store for any safety hazards, either to crew, guests, or food, and react appropriately.</li>
+                        <li>Monitor the store for any safety hazards, either to crew, guests, or food, and react swiftly and appropriately.</li>
                         <li>Deliver positive service for all guests and handle complaints or disputes.</li>
+                        <li>Create a plan to execute a shift and adjust in real-time according to sales trends.</li>
                         <li>Ensure that all crew are provided breaks without interrupting the flow of service for guests.</li>
                         <li>Organize the completion of all tasks so that the next shift manager takes over a clean and stocked restaurant with no outstanding issues.</li>
                         <li>Handle money from the tills and the safe to accurately tally and record daily sales.</li>
@@ -83,14 +90,8 @@ function MyCv()
             <section id="education">
                 <h2>Education</h2>
                 <div className="interval">
-                    <h3>Highschool</h3>
-                    <span>Adam Scott C & V.I., Peterborough, ON</span>
-                    <span>GPA 93.26%</span>
-                </div>
-                <div className="interval">
-                    <h3>University</h3>
+                    <h3>B.Sc. Computer Sciences</h3>
                     <span>Trent University, Peterborough, ON</span>
-                    <span>B.Sc. Computer Sciences</span>
                     <span>GPA 88.90%</span>
                 </div>
             </section>
