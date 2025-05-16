ea9d08afc39b9ffe0f66b8005356ce70c8567977
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-05-16 15:25:47 -0400

    Finish alert & window modes for Popups

--- MyWebsite/src/components/CutoffParagraph.tsx
+++ MyWebsite/src/components/CutoffParagraph.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import { PropsWithChildren } from "react";
 
+import Popup from "./Popup";
+
 type props_CutoffParagraph = {
     id: string
 }
@@ -10,10 +12,11 @@ function CutoffParagraph({id, children}: PropsWithChildren<props_CutoffParagraph
     // Set up the state
     const [fullText, setFullText] = useState<string>("");
     const [cutText, setCutText] = useState<string>("");
+    const [showFullText, setShowFullText] = useState<boolean>(false);
 
     // Cut the text
     useEffect(() => {
-        
+
         // Error checking
         if (typeof children !== "string")
         {
@@ -24,11 +27,18 @@ function CutoffParagraph({id, children}: PropsWithChildren<props_CutoffParagraph
         setCutText(children.split('.', 1)[0] + "...");
     }, []);
 
+    // Show the full text
+    const handleShowText = (e: any) => {
+        e.preventDefault();
+        setShowFullText(true);
+    }
+
     return(
         <div id="cutoff_paragraph">
-            <p id={id}>
+            <p id={id} onClick={handleShowText}>
                 {cutText}
             </p>
+            <Popup type="window" display={showFullText} setDisplay={setShowFullText}>{fullText}</Popup>
         </div>
     )
 }
--- MyWebsite/src/components/Popup.tsx
+++ MyWebsite/src/components/Popup.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { PropsWithChildren } from "react";
 
 import '../styles/Popup.css';
 
+/* Currently unimplemented - set custom style using these values for customizable popups */
 export type PopupDimensions = {
     width?: number,
     height?: number,
@@ -18,18 +19,27 @@ export type PopupDimensions = {
 
 type props_Popup = {
     type: "alert" | "window" | PopupDimensions,
-    display: boolean
+    display: boolean,
+    setDisplay?: Function
 }
 
-function Popup({type, display,children}: PropsWithChildren<props_Popup>)
+function Popup({type, display, setDisplay = () => {}, children}: PropsWithChildren<props_Popup>)
 {
     // Track whether popup should be open or closed
     const [open, setOpen] = useState<boolean>(display);
 
+    // Update whether the popup is open whenever the parent display control changes
+    useEffect(() => {
+        setOpen(display);
+    }, [display]);
+
     // Close the popup
+    // The Popup maintains its own `open` boolean for cases wherein a popup should only ever be show once (like alert mode)
+    // If no setDisplay function is passed, then the Popup can never re-open
     const handleClose = (e: any) => {
         e.preventDefault();
         setOpen(false);
+        setDisplay(false);
     }
 
     return(
