fd5c6da4cdf176261cef3616715dedfa2bb8affd
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-05-16 10:48:51 -0400

    Add page styling for Projects page + GitHub link

--- MyWebsite/src/routes/Projects.tsx
+++ MyWebsite/src/routes/Projects.tsx
@@ -1,6 +1,18 @@
+import ProjectCard from "../components/page/ProjectCard";
+
+import "../styles/Projects.css";
+
 function Projects()
 {
-    return(<p>Hello Projects</p>);
+    return(
+        <div id="projects">
+            <ProjectCard title="My GitHub" icon="github.png" repo="https://github.com/SwishyFishy">
+                Lorem ipsum dolor sit amet and so on and so forth, consectitur and something-or-other.
+                Lorem ipsum dolor sit amet and so on and so forth, consectitur and something-or-other.
+                Lorem ipsum dolor sit amet and so on and so forth, consectitur and something-or-other.
+            </ProjectCard>
+        </div>
+    );
 }
 
 export default Projects;
