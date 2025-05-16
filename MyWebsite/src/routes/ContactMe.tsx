9f9db161cfcfbf9dc50e929143d281353a21fb76
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-05-16 09:29:48 -0400

    CLeanup

--- MyWebsite/src/routes/ContactMe.tsx
+++ MyWebsite/src/routes/ContactMe.tsx
@@ -11,12 +11,14 @@ type formValues = {
     message: string
 }
 
+const initialValues: formValues = {name: '', email: '', message: ''};
+
 function ContactMe()
 {
     return(
         <div id="contact_me">
             <h2>Contact Me</h2>
-            <Form<formValues> initialValues={{name: '', email: '', message: ''}}>
+            <Form<formValues> initialValues={initialValues}>
                 <FormTextInput label="Name" id="name" placeholder="Your name" isArea={false}/>
                 <FormEmailInput label="Email" id="email" placeholder="Your email"/>
                 <FormTextInput label="Message" id="message" placeholder="Your message" isArea={true}/>
