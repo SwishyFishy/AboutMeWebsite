9db388e6ed04ea21ea4ed92255dcc7f68b25513d
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-05-16 10:47:29 -0400

    Add ProjectCard component

---
+++ MyWebsite/src/components/page/ProjectCard.tsx
@@ -0,0 +1,25 @@
+import { PropsWithChildren } from "react";
+
+import "../../styles/ProjectCard.css";
+
+type props_ProjectCard = {
+    title: string,
+    icon: string,
+    repo: string
+}
+
+function ProjectCard({title, icon, repo, children}: PropsWithChildren<props_ProjectCard>)
+{
+    return(
+        <div id="project_card">
+            <h2>{title}</h2>
+            <img src={icon} alt={`Thumbnail image for ${title} repository`} width="230" height="225"/>
+            <div>
+                {children}
+            </div>
+            <a rel="external" target="__blank" href={repo}>Repository</a>
+        </div>
+    );
+}
+
+export default ProjectCard;
