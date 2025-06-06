144b6eabf298b13fe378db53f2773206ebdc3e33
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-06-06 16:11:00 -0400

    Clarify content

--- MyWebsite/src/static_content/AboutMe_Main.tsx
+++ MyWebsite/src/static_content/AboutMe_Main.tsx
@@ -12,7 +12,7 @@ export function AboutMe_Main()
                 I attended Trent University for 4 years between 2021 and 2025, graduating with a B.Sc. in Computer Sciences and an overall GPA just shy of 90%. While obviously I mainly studied computer science, I also took several math and philosophy courses to fill out my electives. During that time, in addition to my job at JCI, I also ran the institutiuon's Magic: the Gathering club. I rebuilt it from the shell that I inherited upon my arrival into a small but flourishing community by the time I left.
             </p>
             <p>
-                I'm currently working at JCI McDonalds as a full-time shift manager. I started in June of 2021, and reached my current position in January of 2024. I've become something of a specialist - informally - regarding my home location's sister store across the street in a Walmart supercenter. There, I have very few team members and a lot of responsibilities to attend to, ensuring that we don't stay late and everything is ready for the next day's open. I'm proud to say that I'm very good at it.
+                I'm currently working at JCI McDonalds as a full-time shift manager. I started in June of 2021, and reached my current position in January of 2024. I've become something of a specialist - informally - regarding my home location's sister store across the street in a Walmart supercenter. Because its located inside the Walmart Supercenter, and is only a few minute's walk from a main storefront location, the margins on its profitability are razor-thin. There are a lot of tasks to be completed, and (relatively speaking) very little staff to make those things happen. I'm proud to say that I;m very good at it.
             </p>
         </>
     );
