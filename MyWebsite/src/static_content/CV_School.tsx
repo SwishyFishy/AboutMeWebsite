dc67145dc3953b91fbbb78550714e15589a7119f
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-06-09 11:42:29 -0400

    Scholarship and honour roll information

--- MyWebsite/src/static_content/CV_School.tsx
+++ MyWebsite/src/static_content/CV_School.tsx
@@ -5,6 +5,9 @@ export function CV_School()
             <h3>B.Sc. Computer Sciences</h3>
             <span>Trent University, Peterborough, ON</span>
             <span>GPA 88.90%</span>
+            <span>Trent National Renewable Scholarship</span>
+            <span>President's Honour Roll</span>
+            <span>Dean's Honour Roll</span>
         </div>
     );
 }
