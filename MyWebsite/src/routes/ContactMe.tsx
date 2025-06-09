18d2fd4111b5a472370153e5335025ee492e524d
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-06-09 11:56:42 -0400

    Better styling for non-desktop screens

--- MyWebsite/src/routes/ContactMe.tsx
+++ MyWebsite/src/routes/ContactMe.tsx
@@ -19,27 +19,28 @@ function ContactMe()
     return(
         <div id="contact_me">
             <h2>Contact Me</h2>
-            <Form<FormValues> initialValues={initialValues} formspree="xgvkrygb">
-                <FormTextInput label="Name" id="name" placeholder="Your name" isArea={false}/>
-                <FormEmailInput label="Email" id="email" placeholder="Your email"/>
-                <FormTextInput label="Message" id="message" placeholder="Your message" isArea={true}/>
-                <FormSubmit value="Send"/>
-            </Form>
-            <section>
-                <h3>Or contact me via my online presence:</h3>
-                <ExternalLink href="https://github.com/SwishyFishy">
-                    <span>
-                        <i className="fa-brands fa-github"/>
-                        SwishyFishy on GitHub
-                    </span>
-                </ExternalLink>
-                <ExternalLink href="https://discord.com/users/sw1shyfishy">   
-                    <span>
-                        <i className="fa-brands fa-discord"/>
-                        sw1shyfishy on Discord
-                    </span>
-                </ExternalLink>
-            </section>
+            <div>
+                <Form<FormValues> initialValues={initialValues} formspree="xgvkrygb">
+                    <FormTextInput label="Name" id="name" placeholder="Your name" isArea={false}/>
+                    <FormEmailInput label="Email" id="email" placeholder="Your email"/>
+                    <FormTextInput label="Message" id="message" placeholder="Your message" isArea={true}/>
+                    <FormSubmit value="Send"/>
+                </Form>
+                <section>
+                    <ExternalLink href="https://github.com/SwishyFishy">
+                        <span>
+                            <i className="fa-brands fa-github"/>
+                            SwishyFishy on GitHub
+                        </span>
+                    </ExternalLink>
+                    <ExternalLink href="https://discord.com/users/sw1shyfishy">
+                        <span>
+                            <i className="fa-brands fa-discord"/>
+                            sw1shyfishy on Discord
+                        </span>
+                    </ExternalLink>
+                </section>
+            </div>
         </div>
     );
 }
